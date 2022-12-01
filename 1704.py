c8d2a85d0612d5559f6346600108e35199e604ba
KyleRiggen <kyle.riggenbach@gmail.com>
2022-11-30 21:13:59 -0700

    adding comments to 1704

--- 1704.py
+++ 1704.py
@@ -1,9 +1,24 @@
 vowels = "aeiouAEIOU"
 
 class Solution:
+
+    # S is word given, bool means function will return a boolean only
     def halvesAreAlike(self, S: str) -> bool:
+
+        # // is floor division
+        # mid is finding middle of word
+        # ans is set to zero, to be increased later
         mid, ans = len(S) // 2, 0
+
+        # range(start, stop, step) starting at middle of word
         for i in range(mid):
+
+            # increase answer if specific letter is a vowel
             if S[i] in vowels: ans += 1
+
+            # decrease answer if specific letter on same side of word is a vowel
             if S[mid+i] in vowels: ans -=1
+        
+        # return true if answer is 0, so each side has matching vowels
+        # otherwise return fasle if answer is not zero
         return ans == 0
