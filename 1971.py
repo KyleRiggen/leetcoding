30eb1d1074ed262c64b936dd1caddf8594b5fbe0
KyleRiggen <kyle.riggenbach@gmail.com>
2022-12-19 17:51:08 -0700

    created 841

--- 1971.py
+++ 1971.py
@@ -1,15 +1,30 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+        #
         adj = [[] for i in range(n)]
+
+        #
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
 
+        # set to keep track of visited
         visit = set()
+
+        # temp variable
         q = [source]
+
+        # adding starting point to visited
         visit.add(source)
+
+        # while q is not empty
         while q != []:
+
+            # giving node the first item in q
             node = q.pop(0)
+
+            # if our starting point is same as destination, return true
             if node == destination:
                 return True
 
