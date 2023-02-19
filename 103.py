b91f2a971cfab20a0d8da61c0feb454cbea81e4f
KyleRiggen <kyle.riggenbach@gmail.com>
2023-02-18 19:05:06 -0700

    Create 103.py

---
+++ 103.py
@@ -0,0 +1,19 @@
+def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+		if not root:
+			return []
+		level = 1
+		res, curr, nxt = [], [root], []
+		while curr:
+			for i in curr:
+				if i.left:
+					nxt.append(i.left)
+				if i.right:
+					nxt.append(i.right)
+			if level%2 == 0:
+				curr.reverse()
+			res.append([i.val for i in curr])
+			curr = nxt
+			if nxt:
+				level +=1
+			nxt = []
+		return res
